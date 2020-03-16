48a87cce1142668cbe1773b541fb69b55796302c
paulwacquet <paulwacquet@outlook.com>
2020-03-16 15:19:54 +0100

    [+] Set-up first commit [-] nothing [~] nothing

---
+++ SDEE/SDEE/DesktopEnvironment.cs
@@ -0,0 +1,64 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Win32;
+
+namespace SDEE
+{
+    class DesktopEnvironment
+    {
+        public Collection<Drawable> GuiElements { get; } = new Collection<Drawable>();
+        public Color Wallpaper { get; set; }
+
+        public DesktopEnvironment(Color wallpaper)
+        {
+            Wallpaper = wallpaper;
+        }
+
+        public void Start()
+        {
+            using (RenderWindow window = new RenderWindow(VideoMode.DesktopMode, null, Styles.Default))
+            {
+                #region Win32 conf
+                IntPtr hWnd = window.SystemHandle;
+                //User.SetWindowLong(hWnd, User.GWL_EXSTYLE, User.GetWindowLong(hWnd, User.GWL_EXSTYLE) & ~User.WS_EX_TOPMOST);
+                //User.SetWindowLong(hWnd,
+                  //  User.GWL_STYLE, User.GetWindowLong(hWnd, User.GWL_STYLE) & ~User.WS_OVERLAPPEDWINDOW);
+                //User.SetWindowPos(hWnd, new IntPtr(1), 0, 0, 0, 0, User.SWP_NOSIZE | User.SWP_NOMOVE | User.SWP_NOACTIVATE);
+
+                #endregion
+
+                window.Closed += (s, e) => window.Close();
+
+                MainLoop();
+
+                void MainLoop()
+                {
+                    while (window.IsOpen)
+                    {
+                        window.DispatchEvents();
+                        window.Clear(Wallpaper);
+
+                        DrawGuiElements();
+
+                        window.Display();
+                    }
+
+                    void DrawGuiElements()
+                    {
+                        foreach (var guiElement in GuiElements)
+                        {
+                            window.Draw(guiElement);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SDEE/SDEE/Program.cs
@@ -0,0 +1,22 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var de = new DesktopEnvironment(new Color(0, 0x80, 0b10000000));
+
+            MyTaskbar myTaskbar = new MyTaskbar(0.05f, new Color(0xC0, 0xC0, 0xC0));
+            de.GuiElements.Add(myTaskbar);
+
+            de.Start();
+        }
+    }
+}
