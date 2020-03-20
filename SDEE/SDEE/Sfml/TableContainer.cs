143718c3ff3a87a63ae72e6c8b6c640f06c1202e
paulwacquet <paulwacquet@outlook.com>
2020-03-20 21:29:10 +0100

    le stash ne fontionne pas

---
+++ SDEE/SDEE/Sfml/TableContainer.cs
@@ -0,0 +1,21 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDEE.Sfml
+{
+    class TableContainer : Control
+    {
+        public int RowCount { get; set; }
+        public int ColumnCount { get; set; }
+        public int BorderSize { get; set; }
+
+        public TableContainer(Control parent) : base(parent)
+        {
+
+        }
+    }
+}
